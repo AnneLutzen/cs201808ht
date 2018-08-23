d97754faa42a668ade41fcb0e4d872dd4b4d298d
AnneLutzen <anne.h.k.lutzen@gmail.com>
2018-08-23 15:42:01 +0200

    Opgave om variabler tilføjet.

---
+++ VariabelOpgave/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariabelOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heltal = 10;
+            heltal++;
+            heltal--;
+            heltal += 20;
+            Console.WriteLine(heltal);
+
+            double kommatal = 12.5;
+            kommatal++;
+            kommatal--;
+            kommatal *= 2;
+            Console.WriteLine(kommatal.ToString("N"));
+
+
+            Console.ReadKey();
+        }
+    }
+}
--- Variabler/Program.cs
+++ Variabler/Program.cs
@@ -10,9 +10,61 @@ namespace Variabler
     {
         static void Main(string[] args)
         {
-            // test 
-            // test2
-            // test3
+            int _nummer;
+            int Nummer;
+            int nummer;
+            int AntalMånederPrÅr;
+
+            //
+            int antalMaanederPrAar = 0;
+            //antalMaanederPrAar = 0;
+            antalMaanederPrAar = antalMaanederPrAar + 1;
+
+            int a = 1, b = 2, c = 3;
+
+            System.Int32 a1 = 1;
+            int a2 = 1;
+
+            //byte w = 300;
+
+            Console.WriteLine(a1);
+            Console.WriteLine("a1 = " + a1);
+            Console.WriteLine($"a1 = {a1}");
+
+
+            int y = 10;
+
+            // instans medlem
+            Console.WriteLine(y.ToString());
+            // statisk medlem
+            Console.WriteLine(int.MaxValue);
+
+            float q = 232.3F;
+
+            int u = 10;
+            u = u - 1;
+            u = u + 1;
+            u = 10;
+            int res = u % 2;
+            Console.WriteLine(res);
+
+            u += 5;
+
+            double tal = 23456678.4678;
+            Console.WriteLine(tal);
+            Console.WriteLine(tal.ToString());
+            Console.WriteLine(tal.ToString("N2"));
+            Console.WriteLine(tal.ToString("N5"));
+            Console.WriteLine(tal.ToString("F2"));
+
+
+
+
+
+
+
+
+            Console.ReadKey();
 
         }
     }
