2f7e2508f546e80972d27e52120d21c9e8e5f35a
AnneLutzen <anne.h.k.lutzen@gmail.com>
2018-08-24 11:00:47 +0200

    Tilføjet opgaver.

---
+++ DatoOpgave/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatoOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+
+            d1 = d1.AddDays(10);
+            Console.WriteLine(d1);
+
+            d1 = d1.AddMinutes(20);
+            Console.WriteLine(d1);
+
+            d1 = d1.AddMonths(-1);
+            Console.WriteLine(d1);
+
+            DateTime d2;
+            d2 = new DateTime(2018, 06, 21, 03, 00, 00);
+            DateTime d3;
+            d3 = new DateTime(2020, 12, 24, 08, 09, 02);
+            Console.WriteLine(d3.Subtract(d2).TotalDays);
+
+            TimeSpan t2;
+            t2 = new TimeSpan(16, 00, 00);
+            Console.WriteLine(t2);
+
+            TimeSpan t3;
+            t3 = new TimeSpan(00, 30, 00);
+            Console.WriteLine(t3);
+
+            TimeSpan t4 = t2.Subtract(t3);
+            Console.WriteLine(t4);
+
+            TimeSpan t5 = t2.Add(t3);
+            Console.WriteLine(t5);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ EnumOpgave/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumOpgave
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+            FilTyper ft = FilTyper.cvs;
+            Console.WriteLine(ft);
+            Console.WriteLine((int)ft);
+
+
+            Person p;
+            p.Id = 1;
+            p.Navn = "Mikkel";
+            Console.WriteLine(p.Navn);
+
+            Console.ReadKey();
+        }
+    }
+
+    struct Person
+    {
+        public int Id;
+        public string Navn;
+
+    }
+
+    public enum FilTyper
+    {
+        cvs = 1,
+        pdf = 2,
+        txt = 3
+    }
+}
