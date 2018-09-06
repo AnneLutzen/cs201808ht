c53472d81b125392e1b6b13879f2235645974723
Anne <admin@TA-826-04>
2018-09-06 15:50:12 +0200

    flere opgaver

---
+++ Arrays_simpel/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_simpel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] månedsløn = { 10000, 20000, 15000, 20000, 30000, 15000 };
+
+            int sum = 0;
+
+            for (int i= 0; i < månedsløn.Length; i++)
+            {
+                sum += månedsløn[i];
+                Console.WriteLine(månedsløn[i]);
+            }
+
+            double gennemsnit = sum / månedsløn.Length;
+            Console.WriteLine("Gennemsnit " + gennemsnit.ToString("N2"));
+
+            Console.ReadKey();
+        }
+    }
+}
