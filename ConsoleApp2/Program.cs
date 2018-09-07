58ac453219a217de02d9e9021f907255cd1b6d64
Anne <admin@TA-826-04>
2018-09-07 15:53:31 +0200

    lalal

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbFunktioner[] a = new IDbFunktioner[4];
+            a[0] = new Hund { Navn = "Niels" };
+            a[1] = new Ubåd { Nummer = 1, Tubine = 2 };
+            a[2] = new Hund { Navn = "HundSød" };
+            a[3] = new Ubåd { Nummer = 3, Tubine = 4 };
+
+            foreach (var item in a)
+            {
+                item.Gem();
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    interface IDbFunktioner
+    {
+        void Gem();
+    }
+
+    class Hund:IDbFunktioner
+    {
+
+        public string Navn { get; set; }
+
+        public void Gem()
+        {
+            Console.WriteLine("Gemmer hund..");
+        }
+    }
+
+    class Ubåd:IDbFunktioner
+    {
+
+        public int Nummer { get; set; }
+        public double Tubine { get; set; }
+
+        public void Gem()
+        {
+            Console.WriteLine("Gemmer ubåd..");
+        }
+    }
+}
